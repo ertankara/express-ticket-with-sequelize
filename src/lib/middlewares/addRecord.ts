e6cf660a9e24f7602daf18d438a30df498ec7fa6
Ertan Kara <ertan.kara@35inch.com>
2020-02-18 00:39:07 +0300

    Update return values

--- src/lib/middlewares/addRecord.ts
+++ src/lib/middlewares/addRecord.ts
@@ -5,12 +5,12 @@ import { Model } from "sequelize";
  * Creates a row in database for the given `Model` e.g if User is passed as model
  * then creates a user row in the Users table
  * @param model Sequelize model object
- * @returns { }
  */
 const addRecord = <M extends Model>(
   model: { new (): M } & typeof Model
 ) => async (req: Request, res: Response, next: NextFunction) => {
-  await model.create(req.body);
+  const newRecord = await model.create(req.body);
+  res.addedRecordResults = { newRecord };
   next();
 };
 
