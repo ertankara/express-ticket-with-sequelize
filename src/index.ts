25d31e3ba634b43f15aa1b7fe852ef085a1e07ed
Ertan Kara <ertan.kara@35inch.com>
2020-02-18 01:18:00 +0300

    _Carry index.ts into top folder with types
    Author:    Ertan Kara <kara.ertan9@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import getPaginatedRecords from "./lib/middlewares/getPaginatedRecords";
+import createRecord from "./lib/middlewares/addRecord";
+import deleteRecord from "./lib/middlewares/deleteRecord";
+import updateRecord from "./lib/middlewares/updateRecord";
+
+export { getPaginatedRecords, createRecord, deleteRecord, updateRecord };
--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import getPaginatedRecords from "./middlewares/getPaginatedRecords";
-import createRecord from "./middlewares/addRecord";
-import deleteRecord from "./middlewares/deleteRecord";
-import updateRecord from "./middlewares/updateRecord";
-
-export { getPaginatedRecords, createRecord, deleteRecord, updateRecord };
