5da921fbc9634738bc87f2295f121f537a465a30
Ertan Kara <kara.ertan9@gmail.com>
2020-02-22 23:21:37 +0300

    createRecord sequelize function can retireve values from req.params too

--- src/lib/middlewares/createRecord.ts
+++ src/lib/middlewares/createRecord.ts
@@ -3,6 +3,8 @@ import { Model as TSSequelizeModel } from "sequelize-typescript";
 import { Model as SequelizeModel } from "sequelize";
 import { LOCAL_CREATED } from "../utils/constants";
 
+const FUNCTION_NAME = "createRecord()";
+
 /**
  * Creates a row in database for the given `Model` e.g if User is passed as model
  * then creates a user row in the Users table, relies on `req.body`
@@ -11,9 +13,28 @@ import { LOCAL_CREATED } from "../utils/constants";
 const createRecord = <M extends TSSequelizeModel, K extends SequelizeModel>(
   model:
     | ({ new (): M } & typeof TSSequelizeModel)
-    | ({ new (): K } & typeof SequelizeModel)
+    | ({ new (): K } & typeof SequelizeModel),
+  requestParams: { paramName: string; passAs?: string }[]
 ) => async (req: Request, res: Response, next: NextFunction) => {
-  const newRecord = await model.create(req.body);
+  // Additional data may be required to collect from the req.params
+  // So if user provided a list of the data to collect from the req.params
+  // look for them in the req.params object
+
+  const valuesFromParams: Record<string, any> = {};
+  for (const requestParam of requestParams) {
+    const paramValue = req.params[requestParam.paramName];
+    if (!paramValue) {
+      throw new Error(
+        `${FUNCTION_NAME} looking for '${requestParam.paramName}' in the req.params but fails to find`
+      );
+    }
+
+    valuesFromParams[
+      requestParam.passAs || requestParam.paramName
+    ] = paramValue;
+  }
+
+  const newRecord = await model.create({ ...req.body, ...valuesFromParams });
   res.locals[LOCAL_CREATED] = { newRecord };
   next();
 };
